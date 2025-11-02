6d0e66bf783e8ec14dfa72f8a777950065e45da0
hyeonjune <annhj980@naver.com>
2025-11-02 23:14:12 +0900

    feat: 랜덤 로또 번호 출력기능 구현

--- src/main/kotlin/lotto/view/LottoOutputView.kt
+++ src/main/kotlin/lotto/view/LottoOutputView.kt
@@ -1,9 +1,16 @@
 package lotto.view
 
+import lotto.model.Lotto
 import lotto.util.LottoConstants.BUYING_TICKET_MESSAGE
 
 class LottoOutputView {
     fun printTicket(ticket: Int) {
         println("${ticket}" + BUYING_TICKET_MESSAGE)
+    }
+
+    fun printLottos(lottos: List<Lotto>) {
+        for (lotto in lottos) {
+            println(lotto.getNumbers())
+        }
     }
 }
